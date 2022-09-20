682e30629daa014811ec47eb3cbce35d2c63807c
Mila Kostic <milakostic.eng@gmail.com>
2022-09-20 08:56:14 +0200

    exaples - querySelector()

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ console.log(button);
 
 const p_tag = document.getElementsByTagName("p");
 console.log(p_tag);
+
+/*Select elements by using CSS Selectors*/
+
+const img = document.querySelector("img"); // select element by tag name
+const input = document.querySelector("input[type='text']");
+last_div = document.querySelector("form > *:last-child");
+const button = document.querySelector(".btn"); //select element by class name
+const button = document.querrySelector("#btn"); //select element by id
